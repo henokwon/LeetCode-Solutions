50cbd59491bdb72409be3eab16900a41013c2833
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2023-11-26 23:45:50 +0300

    Time: 48 ms (78.64%) | Memory: 42 MB (69.65%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.js
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -5,13 +5,9 @@
  */
 var mergeAlternately = function(word1, word2) {
     let result = '';
-    let i = 0;
-    let j = 0;
-
-    while (i < word1.length || j < word2.length) {
-        if (i < word1.length) result += word1[i++];
-        if (j < word2.length) result += word2[j++];
+    for (let i = 0, j = 0; i < word1.length || j < word2.length; i++, j++) {
+        if (i < word1.length) result += word1[i];
+        if (j < word2.length) result += word2[j];
     }
-
     return result;
 };
