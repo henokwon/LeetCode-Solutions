f940f51250cc67109df8c57a7b17fffba7cf1a72
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2023-11-29 22:49:30 +0300

    Time: 94 ms (35.42%) | Memory: 47.2 MB (63.51%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.js
+++ 2-add-two-numbers/add-two-numbers.js
@@ -10,7 +10,9 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {let dummyHead = new ListNode(0);
+var addTwoNumbers = function(l1, l2) {
+    
+    let dummyHead = new ListNode(0);
     let current = dummyHead;
     let carry = 0;
 
@@ -23,8 +25,8 @@ var addTwoNumbers = function(l1, l2) {let dummyHead = new ListNode(0);
         current.next = new ListNode(sum % 10);
         current = current.next;
 
-        l1 = l1 ? l1.next : null;
-        l2 = l2 ? l2.next : null;
+        if (l1) l1 = l1.next;
+        if (l2) l2 = l2.next;
     }
 
     return dummyHead.next;
