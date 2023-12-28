eccdbf873e884d90f40d2fa7e99bb70c56612cea
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2023-12-28 13:45:10 +0300

    Time: 84 ms (5.59%) | Memory: 41.4 MB (94.38%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -6,7 +6,9 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let i = m - 1, j = n - 1, k = m + n - 1;
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
 
     while (i >= 0 && j >= 0) {
         nums1[k--] = (nums1[i] > nums2[j]) ? nums1[i--] : nums2[j--];
