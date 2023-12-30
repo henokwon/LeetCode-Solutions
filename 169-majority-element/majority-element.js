1ebf1648a4d9a48ff30b4c0e3ef02fd7881cb690
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2023-12-30 23:43:30 +0300

    Time: 73 ms (12.46%) | Memory: 43.4 MB (75.53%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -4,17 +4,17 @@
  */
 var majorityElement = function(nums) {
     let candidate = 0;
-    let  candidateCount = 0;
+    let  candidateC = 0;
     let i = 0;
 
     while (i < nums.length) {
-        if (candidateCount === 0) {
+        if (candidateC === 0) {
             candidate = nums[i];
-            candidateCount = 1;
+            candidateC = 1;
         } else if (candidate === nums[i]) {
-            candidateCount++;
+            candidateC++;
         } else {
-            candidateCount--;
+            candidateC--;
         }
         i++;
     }
