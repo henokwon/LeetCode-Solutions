eb60d49e9271ce0c328036f1b2aa6d111c330b8d
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2023-12-30 22:19:25 +0300

    Time: 124 ms (9.95%) | Memory: 44.9 MB (29.25%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let k = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[k - 1]) nums[k++] = nums[i];
+    }
+    return k;
+};
