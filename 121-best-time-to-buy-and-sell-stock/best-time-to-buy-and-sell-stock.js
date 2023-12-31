653d57ed746ec87b78124c0bb76e5362b20c8a78
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2024-01-01 00:00:58 +0300

    Time: 160 ms (5.13%) | Memory: 51.5 MB (58.98%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -4,13 +4,11 @@
  */
 var maxProfit = function(prices) {
   
- let maxProfit = 0;
-  let buy = prices[0];
-
-  for (let i = 1; i < prices.length; i++) {
-    buy = Math.min(buy, prices[i]);
+ let maxProfit = 0, buy = prices[0];
+  
+  for (let i = 1; i < prices.length; i++)
+    buy = Math.min(buy, prices[i]),
     maxProfit = Math.max(maxProfit, prices[i] - buy);
-  }
 
   return maxProfit;
 };
