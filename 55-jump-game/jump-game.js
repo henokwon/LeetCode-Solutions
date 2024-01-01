bde7a53568a0c401319eb534a4702bf251e50664
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2024-01-01 23:38:17 +0300

    Time: 65 ms (55.06%) | Memory: 45.9 MB (79.94%) - LeetSync

---
+++ 55-jump-game/jump-game.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    if (nums.length <= 1) {
+        return true;
+    }
+
+    let maximum = nums[0];
+
+    for (let i = 0; i < nums.length; i++) {
+        if (maximum <= i && nums[i] === 0) {
+            return false;
+        }
+
+        if (i + nums[i] > maximum) {
+            maximum = i + nums[i];
+        }
+
+        if (maximum >= nums.length - 1) {
+            return true;
+        }
+    }
+
+    return false;
+};
