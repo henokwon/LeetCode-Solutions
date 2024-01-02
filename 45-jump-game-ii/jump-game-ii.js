7439a7f374a1e9fefe9454d69cd0c3ddb9fd9622
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2024-01-02 22:43:27 +0300

    Time: 94 ms (22.65%) | Memory: 43.4 MB (90.80%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.js
+++ 45-jump-game-ii/jump-game-ii.js
@@ -3,23 +3,22 @@
  * @return {number}
  */
 var jump = function(nums) {
-    let len = nums.length - 1, curr = -1, next = 0, ans = 0;
+    let n = nums.length - 1, c = -1, nxt = 0, res = 0;
 
-    while (next < len) {
-        ans++;
+    while (nxt < n) {
+        res++;
+        const prevC = c;
+        let maxIdx = nxt;
 
-        const prevCurr = curr;
-        let maxIndex = next;
-
-        for (let i = prevCurr + 1; i <= next; i++) {
-            if (i + nums[i] > maxIndex + nums[maxIndex]) {
-                maxIndex = i;
+        for (let i = prevC + 1; i <= nxt; i++) {
+            if (i + nums[i] > maxIdx + nums[maxIdx]) {
+                maxIdx = i;
             }
         }
 
-        curr = maxIndex;
-        next = curr + nums[curr];
+        c = maxIdx;
+        nxt = c + nums[c];
     }
 
-    return ans;
+    return res;
 };
