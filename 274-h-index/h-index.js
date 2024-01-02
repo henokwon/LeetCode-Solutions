35e2e08e1ea0a346cf24ecf94973e9ab68c67b76
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2024-01-02 23:20:03 +0300

    Time: 71 ms (18.08%) | Memory: 42 MB (64.07%) - LeetSync

--- 274-h-index/h-index.js
+++ 274-h-index/h-index.js
@@ -3,12 +3,10 @@
  * @return {number}
  */
 var hIndex = function(citations) {
- citations.sort((a, b) => a - b);
+citations.sort((a, b) => a - b);
     let i = 0;
 
-    while (i < citations.length && citations[i] < citations.length - i) {
-        i++;
-    }
+    while (i < citations.length && citations[i] < citations.length - i) i++;
 
     return citations.length - i;
 };
