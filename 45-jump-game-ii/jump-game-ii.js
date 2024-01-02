941f2e16741cec8ca0bd7b4621ca4d801e31d4ec
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2024-01-02 22:48:54 +0300

    Time: 539 ms (5.02%) | Memory: 44.4 MB (24.82%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.js
+++ 45-jump-game-ii/jump-game-ii.js
@@ -3,18 +3,18 @@
  * @return {number}
  */
 var jump = function(nums) {
-    let n = nums.length - 1, c = -1, nxt = 0, res = 0;
+    const n = nums.length;
+    const dp = new Array(n).fill(Number.MAX_SAFE_INTEGER);
 
-    while (nxt < n) {
-        res++;
-        let maxIdx = nxt;
-        
-        for (let i = c + 1; i <= nxt; i++)
-            if (i + nums[i] > maxIdx + nums[maxIdx]) maxIdx = i;
+    dp[0] = 0;
 
-        c = maxIdx;
-        nxt = c + nums[c];
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            if (j + nums[j] >= i) {
+                dp[i] = Math.min(dp[i], dp[j] + 1);
+            }
+        }
     }
 
-    return res;
+    return dp[n - 1];
 };
