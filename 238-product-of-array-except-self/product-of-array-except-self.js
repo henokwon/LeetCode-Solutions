14f7718f1b954837b748965ed527d41e26001dd8
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2024-01-04 23:59:38 +0300

    Time: 85 ms (72.36%) | Memory: 53.3 MB (80.76%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -2,12 +2,14 @@ var productExceptSelf = function(nums) {
     const n = nums.length;
     const result = Array(n).fill(1);
 
-    for (let i = 0, prefix = 1; i < n; prefix *= nums[i++]) {
+    for (let i = 0, prefix = 1; i < n; i++) {
         result[i] *= prefix;
+        prefix *= nums[i];
     }
 
-    for (let i = n - 1, suffix = 1; i >= 0; suffix *= nums[i--]) {
+    for (let i = n - 1, suffix = 1; i >= 0; i--) {
         result[i] *= suffix;
+        suffix *= nums[i];
     }
 
     return result;
