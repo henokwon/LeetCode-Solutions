a83759efe475e40c43f8bcd8b4861758d55583fa
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2024-01-05 00:03:51 +0300

    Time: 71 ms (75.49%) | Memory: 53.5 MB (47.20%) - LeetSync

---
+++ 134-gas-station/gas-station.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} gas
+ * @param {number[]} cost
+ * @return {number}
+ */
+var canCompleteCircuit = function(gas, cost) {
+    let start = 0, tank = 0, total = 0;
+
+    for (let i = 0; i < gas.length; i++) {
+        const consume = gas[i] - cost[i];
+        tank += consume;
+        total += consume;
+
+        if (tank < 0) {
+            tank = 0;
+            start = i + 1;
+        }
+    }
+
+    return total < 0 ? -1 : start;
+};
