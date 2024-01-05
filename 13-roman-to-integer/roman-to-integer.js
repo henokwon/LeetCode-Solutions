cb85d9cf7272e20f632b4dcd71e1919a121fabb5
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2024-01-05 23:38:18 +0300

    Time: 83 ms (99.55%) | Memory: 47.3 MB (52.29%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.js
+++ 13-roman-to-integer/roman-to-integer.js
@@ -3,28 +3,14 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    const symbols = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    };
-
+    const symbols = {I:1,V:5,X:10,L:50,C:100,D:500,M:1000};
     let total = 0;
-
+    
     for (let i = 0; i < s.length; i++) {
-        const currentSymbol = symbols[s[i]];
-        const nextSymbol = symbols[s[i + 1]];
-
-        if (currentSymbol < nextSymbol) {
-            total += nextSymbol - currentSymbol;
-            i++;
-        } else {
-            total += currentSymbol;
-        }
+        const cur = symbols[s[i]];
+        const next = symbols[s[i + 1]];
+        total += cur < next ? next - cur : cur;
+        if (cur < next) i++;
     }
 
     return total;
