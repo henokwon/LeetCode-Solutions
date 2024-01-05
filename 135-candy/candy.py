03ca28c5a7c747e70d9b9118c42621b0a32a5e65
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2024-01-05 23:22:19 +0300

    Time: 128 ms (90.76%) | Memory: 20.6 MB (12.65%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -3,12 +3,12 @@ class Solution:
         n = len(ratings)
         candies = [1] * n
 
-        # Left to right pass
+    # Left to right pass
         for i in range(1, n):
             candies[i] = candies[i - 1] + 1 if ratings[i] > ratings[i - 1] else 1
 
-        # Right to left pass
+    # Right to left pass
         for i in range(n - 2, -1, -1):
             candies[i] = max(candies[i], candies[i + 1] + 1) if ratings[i] > ratings[i + 1] else candies[i]
 
-        return sum(candies)    
+        return sum(candies)
