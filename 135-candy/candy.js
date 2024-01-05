fced12ce5e4b0b1de9bc81cdeaff431d3b344d06
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2024-01-05 23:13:21 +0300

    Time: 66 ms (50.29%) | Memory: 45.5 MB (34.53%) - LeetSync

--- 135-candy/candy.js
+++ 135-candy/candy.js
@@ -6,17 +6,12 @@ var candy = function(ratings) {
     const n = ratings.length;
     const candies = new Array(n).fill(1);
 
-    // Left to right and right to left pass
     for (let i = 1; i < n; i++) {
-        if (ratings[i] > ratings[i - 1]) {
-            candies[i] = candies[i - 1] + 1;
-        }
+        candies[i] = ratings[i] > ratings[i - 1] ? candies[i - 1] + 1 : 1;
     }
 
     for (let i = n - 2; i >= 0; i--) {
-        if (ratings[i] > ratings[i + 1]) {
-            candies[i] = Math.max(candies[i], candies[i + 1] + 1);
-        }
+        candies[i] = Math.max(candies[i], ratings[i] > ratings[i + 1] ? candies[i + 1] + 1 : 1);
     }
 
     return candies.reduce((a, b) => a + b, 0);
