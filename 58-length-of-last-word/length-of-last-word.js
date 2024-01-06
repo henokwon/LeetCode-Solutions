8acef186ab6c0bcd7ed11f596d14f55e56e9ec78
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2024-01-06 23:05:40 +0300

    Time: 40 ms (97.47%) | Memory: 42 MB (36.72%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -6,15 +6,12 @@ var lengthOfLastWord = function(s) {
     let length = 0;
     let end = s.length - 1;
 
-    // Trim trailing spaces
     while (end >= 0 && s[end] === ' ') {
         end--;
     }
 
-    // Calculate the length of the last word
-    while (end >= 0 && s[end] !== ' ') {
+    while (end >= 0 && s[end--] !== ' ') {
         length++;
-        end--;
     }
 
     return length;
