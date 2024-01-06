3a17f89e7d232874ae091c4ac63f39a73aec430b
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2024-01-06 23:28:40 +0300

    Time: 63 ms (26.66%) | Memory: 43.2 MB (23.26%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -3,17 +3,13 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    if (strs.length === 0) {
-        return '';
-    }
+    if (strs.length === 0) return '';
 
     let ans = strs[0];
 
     for (let i = 1; i < strs.length && ans.length > 0; i++) {
         let j = 0;
-        while (j < ans.length && j < strs[i].length && ans[j] === strs[i][j]) {
-            j++;
-        }
+        while (j < ans.length && ans[j] === strs[i][j]) j++;
         ans = ans.substring(0, j);
     }
 
