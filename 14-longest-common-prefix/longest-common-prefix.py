4539ea0c6ecee51bfaeb7652a7fda6f587f1bdac
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2024-01-06 23:42:20 +0300

    Time: 39 ms (73.08%) | Memory: 17.4 MB (20.05%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -3,13 +3,13 @@ class Solution:
         if not strs:
             return ''
 
-        prefix = min(strs)
-        i = 0
-
-        while i < len(prefix):
-            if any(string[i] != prefix[i] for string in strs):
-                return prefix[:i]
-            i += 1
+        prefix = ''
+        for chars in zip(*strs):
+            if len(set(chars)) == 1:
+                prefix += chars[0]
+            else:
+                break
 
         return prefix
+
     
