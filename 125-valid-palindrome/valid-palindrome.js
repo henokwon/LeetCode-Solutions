75f042fd42ddbce3f3f135c38fbc14c5d55a0fb2
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2024-01-08 20:35:50 +0300

    Time: 65 ms (54.43%) | Memory: 49.1 MB (17.18%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -3,13 +3,11 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-     if (s.length === 0) return true;
+     if (!s.length) return true;
 
     const isAlphaNumeric = (char) => /[a-zA-Z0-9]/.test(char);
 
-    let left = 0, right = s.length - 1;
-
-    while (left < right) {
+    for (let left = 0, right = s.length - 1; left < right; ) {
         if (!isAlphaNumeric(s[left])) left++;
         else if (!isAlphaNumeric(s[right])) right--;
         else if (s[left++].toLowerCase() !== s[right--].toLowerCase()) return false;
