56804431998a5f2e4bd6e60136a10ae131989100
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2024-01-11 22:31:32 +0300

    Time: 59 ms (26.21%) | Memory: 42 MB (49.99%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -12,16 +12,7 @@ var strStr = function(haystack, needle) {
     }
 
     for (let i = 0; i <= hayLen - needleLen; i++) {
-        let match = true;
-
-        for (let j = 0; j < needleLen; j++) {
-            if (haystack[i + j] !== needle[j]) {
-                match = false;
-                break;
-            }
-        }
-
-        if (match) {
+        if (haystack.slice(i, i + needleLen) === needle) {
             return i;
         }
     }
