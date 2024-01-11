913b33d23a5b2ad492dd44ec632e1006edc9f7a9
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2024-01-11 23:00:08 +0300

    Time: 56 ms (43.74%) | Memory: 42.5 MB (13.11%) - LeetSync

--- 392-is-subsequence/is-subsequence.js
+++ 392-is-subsequence/is-subsequence.js
@@ -4,20 +4,14 @@
  * @return {boolean}
  */
 const isSubsequence = (s, t) => {
-  if (s.length > t.length) {
-    return false;
-  }
-
   let subsequence = 0;
+  
   for (let char of t) {
-    if (s[subsequence] === char) {
-      subsequence++;
-    }
-
-    if (subsequence === s.length) {
+    if (s[subsequence] === char && ++subsequence === s.length) {
       return true;
     }
   }
 
   return subsequence === s.length;
 };
+
