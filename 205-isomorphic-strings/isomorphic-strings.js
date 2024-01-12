c548e593667f68ebf86ef60a9207c0544f3c4027
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2024-01-12 23:39:24 +0300

    Time: 54 ms (90.77%) | Memory: 42.4 MB (68.95%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.js
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -4,20 +4,14 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-    if (s.length !== t.length) {
-        return false;
-    }
+    if (s.length !== t.length) return false;
 
-    const map1 = new Array(256).fill(0);
-    const map2 = new Array(256).fill(0);
+    const map1 = Array(256).fill(0);
+    const map2 = Array(256).fill(0);
 
     for (let i = 0; i < s.length; i++) {
-        if (map1[s.charCodeAt(i)] !== map2[t.charCodeAt(i)]) {
-            return false;
-        }
-        
-        map1[s.charCodeAt(i)] = i + 1;
-        map2[t.charCodeAt(i)] = i + 1;
+        if (map1[s.charCodeAt(i)] !== map2[t.charCodeAt(i)]) return false;
+        map1[s.charCodeAt(i)] = map2[t.charCodeAt(i)] = i + 1;
     }
 
     return true;
