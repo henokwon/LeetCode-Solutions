5464daac5eb1951ff69554d70453e591cd901d49
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2024-01-14 21:48:24 +0300

    Time: 63 ms (69.85%) | Memory: 18.6 MB (9.71%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,7 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         num_index_map = {}
-    
+
         for current_index, current_num in enumerate(nums):
             complement = target - current_num
 
@@ -9,4 +9,3 @@ class Solution:
                 return [current_index, num_index_map[complement]]
 
             num_index_map[current_num] = current_index
-    
