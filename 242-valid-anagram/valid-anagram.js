3cf847cda7286aa1741eb503c421fe36152af6d4
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2024-01-14 21:34:44 +0300

    Time: 56 ms (95.13%) | Memory: 42.2 MB (94.34%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+    
+    var freq = new Array(26).fill(0);
+    
+    for (var i = 0; i < s.length; i++) {
+        freq[s.charCodeAt(i) - 97]++;
+        freq[t.charCodeAt(i) - 97]--;
+    }
+    
+    for (var i = 0; i < freq.length; i++) {
+        if (freq[i] !== 0) {
+            return false;
+        }
+    }
+    
+    return true;
+};
