9bbc7ff6f9d229fc0f8c6663e5c22eff63440db4
Henok Wondu <96754775+henokwon@users.noreply.github.com>
2025-02-15 20:51:25 +0300

    Time: 27 ms (15.17%) | Memory: 53 MB (38.63%) - LeetSync

---
+++ 69-sqrtx/sqrtx.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    if(x == 0 || x == 1)
+        return x
+    for(let i=0; i <= x; i++){
+        if (i*i > x)
+        return i - 1
+    }
+};
